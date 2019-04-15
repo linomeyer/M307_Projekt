51279e53a85de7c38648d93be5955a30db374891
lino <lino.bucher@gmail.com>
2019-04-15 15:00:44 +0200

    edit-mvc

--- Videothek/routes.php
+++ Videothek/routes.php
@@ -5,5 +5,5 @@ $router = new Router();
 $router->define([
     '' => 'app/Controllers/WelcomeController.php',
     'erfassen' => 'app/Controllers/CaptureController.php',
-
+    'bearbeiten' => 'app/Controllers/EditController.php',
 ]);
