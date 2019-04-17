c0b0d280a0200131ab14f4b0f71e9a4daa2d5a28
lino <lino.bucher@gmail.com>
2019-04-17 09:56:49 +0200

    validation-update

--- Videothek/app/Controllers/ValidationController.php
+++ Videothek/app/Controllers/ValidationController.php
@@ -8,13 +8,13 @@ function validate(string $name, string $firstname, string $email, string $telefo
     $allMemberStatuses = ['keine', 'bronze', 'silber', 'gold'];
 
     if (trim($name) === '') {
-        array_push($errors, 'Namensfeld ist leer!');
+        array_push($errors, 'Nachamensfeld ist leer!');
     }
     if (strlen($name) <= 2) {
         array_push($errors, 'Der Name muss länger als 4 Zeichen sein!');
     }
     if (trim($firstname) === '') {
-        array_push($errors, 'Namensfeld ist leer!');
+        array_push($errors, 'Voramensfeld ist leer!');
     }
     if (trim($email) === '') {
         array_push($errors, 'Emailfeld ist leer!');
@@ -27,9 +27,6 @@ function validate(string $name, string $firstname, string $email, string $telefo
     }
     if (preg_match("/[a-z]/i", $telefon)) {
         array_push($errors, 'Telefonnummer darf keine Alphabetischen Zeichen enthalten!');
-    }
-    if (!in_array($memberStatus, $allMemberStatuses)) {
-        array_push($errors, 'Der Ausgewählte Mitgliedschaftsstatus existiert nicht!');
     }
     return $errors;
 }
