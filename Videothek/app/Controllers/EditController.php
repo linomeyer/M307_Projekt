4bf10fe8c290d96713d747d740c4a92446356dac
NevinJulian <nevinhelfenstein@hotmail.ch>
2019-04-17 13:39:03 +0200

    Edit

--- Videothek/app/Controllers/EditController.php
+++ Videothek/app/Controllers/EditController.php
@@ -1,5 +1,6 @@
 <?php
 require 'app/Views/navbar.view.php';
+require 'app/Controllers/ValidationController.php';
 
 $name = '';
 $email= '';
@@ -14,7 +15,7 @@ if (isset($_GET['id'])) {
     $rent = new RentedMovie();
     $rent = $rent->getRentbyId($id);
 
-
+    $id = (int) $rent['id'];
     $name = $rent['name'];
     $firstname = $rent['firstname'];
     $rentStart = $rent['rentStart'];
@@ -25,11 +26,31 @@ if (isset($_GET['id'])) {
 
     $movies = Movie::getAllMovies();
 
-    
-
+    require 'app/Views/edit.view.php';
 }
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    if(isset($_POST)){
+        $name = $_POST['name'] ?? '';
+        $firstname = $_POST['firstname'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $phone = $_POST['phone'] ?? '';
+        $member = strtolower($_POST['member-status'] ?? '') ;
+        $movie = $_POST['movie'] ?? '1';
+        $enddatum = $_POST['enddate'] ?? '';
+        $rentstart = date("d.m.Y");
+
+        $errors = validate($name, $firstname, $email, $phone, $member, $movie);
+
+        if(count($errors) === 0) {
+            $rentedMovie = new RentedMovie($name, $firstname, $email, $member, $phone, $movie);
+            $rentedMovie->updateRent($id, $member);
+            header('Location: anzeigen');
+        }
+        require 'app/Views/show.view.php';
+    }
+}
+
 else {
     header('Location: anzeigen');
 }
-
-require 'app/Views/edit.view.php';
